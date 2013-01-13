addf5dbe488eadace1a463b74c7f9afa1d3dd9a5
wikitopian <matt@swarmstrategies.com>
2013-01-12 21:55:11 -0700

    menu fix

--- classes/class-mildendo-walker-nav-menu.php
+++ classes/class-mildendo-walker-nav-menu.php
@@ -3,15 +3,15 @@
 class Mildendo_Walker_Nav_Menu extends Walker_Nav_Menu {
 
 	function start_lvl( &$output, $depth ) {
-		$output = '';
+		$output .= '';
 	}
 
 	function end_lvl( &$output, $depth ) {
-		$output = '';
+		$output .= '';
 	}
 
 	function start_el( &$output, $item, $depth ) {
-		$output  = " <a href='{$item->url}' ";
+		$output  .= " <a href='{$item->url}' ";
 		if( $item->attr_title ) {
 			$output .= " data-icon='{$item->attr_title}' ";
 		}
