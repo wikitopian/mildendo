18a49477de0fbffcb66ed8fc05dd4ee5f2925e05
wikitopian <matt@swarmstrategies.com>
2015-07-04 13:44:30 +0000

    add author avatar

--- content.php
+++ content.php
@@ -7,5 +7,26 @@
 		</a>
 	<?php } ?>
 </h2>
-<?php the_content( 'Read more...' ); ?>
-<?php get_template_part( 'meta' ); ?>
+
+<div class="mildendo_gravatar">
+
+<?php
+
+$author_link = get_author_posts_url( get_the_author_meta( 'ID' ) );
+$author_name = get_the_author();
+
+echo "<a href='{$author_link}'>";
+
+echo get_avatar( get_the_author_meta( 'email' ), '64', null, get_the_author() );
+echo "<span>By: {$author_name}</span></a>";
+
+?>
+
+</div>
+
+<?php
+
+the_content( 'Read more...' );
+get_template_part( 'meta' );
+
+/* EOF */
