41670b5e36f63da9384fd581f89b057daa508c29
wikitopian <matt@swarmstrategies.com>
2015-07-04 14:10:58 +0000

    use proper name on avatar

--- content.php
+++ content.php
@@ -1,19 +1,18 @@
-<h2>
+<span>
 	<?php if( !is_single() ) { ?>
 		<a href="<?php the_permalink() ?>" rel="bookmark">
 	<?php } ?>
-			<?php the_title(); ?>
+			<h2><?php the_title(); ?></h2>
 	<?php if( !is_single() ) { ?>
 		</a>
 	<?php } ?>
-</h2>
 
 <div class="mildendo_gravatar">
 
 <?php
 
 $author_link = get_author_posts_url( get_the_author_meta( 'ID' ) );
-$author_name = get_the_author();
+$author_name = get_the_author_meta( 'display_name' );
 
 echo "<a href='{$author_link}'>";
 
@@ -23,6 +22,8 @@ echo "<span>By: {$author_name}</span></a>";
 ?>
 
 </div>
+
+</span>
 
 <?php
 
