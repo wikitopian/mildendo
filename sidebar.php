f62dc47b26b74707fdbc06aec649ae86600016a2
wikitopian <matt@swarmstrategies.com>
2015-07-05 15:57:05 +0000

    no featured image in sidebar on home page

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,14 @@
-<div id="mildendo-header">
-	<a href="<?php echo home_url(); ?>">
-		<img
-			src="<?php header_image(); ?>"
-			alt="Site Logo" />
-	</a>
-</div>
+<?php if( !is_home() ) { ?>
 
+	<div id="mildendo-header">
+		<a href="<?php echo home_url(); ?>">
+			<img
+				src="<?php header_image(); ?>"
+				alt="Site Logo" />
+		</a>
+	</div>
+
+<?php } ?>
 
 <?php dynamic_sidebar( 'opened' ); ?>
 
