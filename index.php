6f59006a5994c6de036e8260c73cc8b5d54a04c6
wikitopian <matt@swarmstrategies.com>
2015-07-13 16:43:41 -0600

    add error dialog, buttons

--- index.php
+++ index.php
@@ -31,6 +31,9 @@
 					<?php if( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+							<?php do_action( 'mildendo_alert' ); ?>
+
 							<?php get_template_part( 'content', get_post_format() ); ?>
 						</article>
 
